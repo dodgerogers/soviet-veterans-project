e435d1f9310f576bf424820b0187524e07f48f5e
Andy <dodgerogers@hotmail.com>
2014-06-21 23:40:40 -0400

    fixtures moved to their own seperate files

---
+++ js/models/story.js
@@ -0,0 +1,69 @@
+// ---------------------------------------------------------------------------------------------------------//
+// ------------------------------------------------- STORIES -----------------------------------------------//
+// ---------------------------------------------------------------------------------------------------------//
+
+App.Story.FIXTURES = [
+// --------------------------------------------- JEWS IN RUSSIA --------------------------------------------//
+//Brief History Of Jews In Russia And The Soviet Union
+	{
+		id: 400,
+		name: "Tsarist Period",
+		date: "1700 - 1917",
+		category: "jews-in-russia"
+	},
+	{
+		id: 401,
+		name: "Jews In The Russian Revolution",
+		date: "1917 - 1920",
+		category: "jews-in-russia"
+	},
+	{
+		id: 402,
+		name: "Jews In The Soviet Union 1920 - 1930",
+		date: "1920 - 1930",
+		category: "jews-in-russia"
+	},
+//World War II Period	
+	{
+		id: 403,
+		name: "Jewish Anti Fascist Committee (JAC)",
+		date: "1942 - 1948",
+		category: "ww2"
+	},
+//Post World War II - Stalin's War Against The Jews 1948 - 1953
+	{
+		id: 404,
+		name: "JAC Executions Crimean Affair and Night Of Murdered Poets",
+		date: "1948 - 1952",
+		category: "post-ww2"
+	},
+	{
+		id: 405,
+		name: "Doctor's Plot",
+		date: "1952 - 1953",
+		category: "post-ww2"
+	},
+// ------------------------------------------------ PARTISANS------------------------------------------------//
+	{
+		id: 410,
+		name: "Underground resistance movements that fought the Nazis",
+		date: "1941 - 1945",
+		category: "partisans",
+		copyright: "nme.com",
+	},
+// --------------------------------------------- FORCES + BADGES --------------------------------------------//
+	{
+		id: 411,
+		name: "SMERSH",
+		date: "1943 - 1946",
+		category: "military-unit",
+		copyright: "deviantart.com",
+	},
+	{
+		id: 412,
+		name: "NKVD",
+		date: "1934 - 1945",
+		category: "military-unit",
+		copyright: "forum.szone-online.net",
+	},
+];
